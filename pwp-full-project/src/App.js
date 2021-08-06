f3c4dda0d43c1d24aa52098665caa4ae2037cfd5
admin <admin@admins-MacBook-Pro.local>
2021-08-05 18:20:32 -0700

    forms

---
+++ pwp-full-project/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import CustomerForm from './Forms/CustomerForm';
+
+
+function App(){
+    return(
+    <div>
+        <CustomerForm/>
+    </div>
+    );
+}
+
+export default App;
---
+++ pwp-full-project/src/Forms/CustomerForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+const CustomerForm = ({ addCustomer }) => {
+  const [customer, setCustomer] = useState({
+    firstName:"",
+    middleName:"",
+    lastName:"",
+    age:-3,
+    contactNo:"",
+    email:"",
+    problem:""
+  });
+
+  const handleChange = (event) => {
+    // setCustomer({ ...customer, firstName: event.target.value });
+    const {name, value } = event.target
+    setCustomer ({
+        [name]:value
+    })
+    
+    // alert(name + ":"+ value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // addTodo(todo);
+    // setCustomer({  ...customer, setCustomer  });
+    // alert(customer.firstName);
+    if(customer.age < 18){
+        alert("You are not of legal age");
+        return;
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        First Name : <input
+          type="text"
+          name="firstName"
+          onChange={handleChange}
+          value={customer.firstName}
+        /><br></br>
+          Middle Name :  <input
+          type="text"
+          name="middleName"
+          onChange={handleChange}
+          value={customer.middleName}
+        /><br></br>
+          Last Name : <input
+          type="text"
+          name="lastName"
+          onChange={handleChange}
+          value={customer.lastName}
+        /><br></br>
+        Age :<input
+          type="text"
+          name="age"
+          onChange={handleChange}
+          value={customer.age}
+        /><br></br>
+        Phone Number :<input
+          type="text"
+          name="contactNo"
+          onChange={handleChange}
+          value={customer.contactNo}
+        /><br></br>
+        Email :<input
+          type="text"
+          name="email"
+          onChange={handleChange}
+          value={customer.email}
+        /><br></br>
+        Problem statement :<TextArea
+                    value={customer.problem}
+                    placeholder="Tell us about your problem"
+                    onChange={handleChange}
+                /><br></br>
+        <input type="submit" value="Submit!" />
+      </form>
+    </div>
+  );
+};
+
+export default CustomerForm;
