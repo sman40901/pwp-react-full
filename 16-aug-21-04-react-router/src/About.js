25dd54ccb74f984011fe7f68b6f0424f3d88182e
sman40901 <sujanmanpr@gmail.com>
2021-08-16 08:50:18 -0700

    router ewip

--- 16-aug-21-04-react-router/src/About.js
+++ 16-aug-21-04-react-router/src/About.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'  
 import './App.css';  
 // import App from './App';  
-import About from './About'  
+// import About from './About'  
 import Contact from './Contact'  
 import Home from './Home'  
   
