329e0f583508aac40904aeee019a6ed7f2193a22
sman40901 <sujanmanpr@gmail.com>
2021-08-17 09:11:42 -0700

    router.js changed and loading.js added

--- 16-aug-21-04-react-router/src/Routes.js
+++ 16-aug-21-04-react-router/src/Routes.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
+import loadable from 'react-loadable';
+
+import LoadingPage from "./LoadingPage";
 import Page404 from "./pages/404Page";
 import LoginPage from "./pages/LoginPage";
 import SubPage from './pages/SubPage';
+
+const AsyncPages = {
+    myAsyncSubPage: loadable({
+        loader: () => import('./pages/myAsyncSubPage'),
+        loading: LoadingPage
+    })
+};
 
 /**
  * Routes component containing routes for the whole application
@@ -12,6 +22,7 @@ const Routes = () => (
     <Switch>
         <Route exact path='/login' component={LoginPage} />
         <Route exact path='/sub-page' component={SubPage} />
+        <Route exact path='/my-async-sub-page' component={AsyncPages.myAsyncSubPage} />
 
         <Redirect exacts from='/' to='/login' />
 
---
+++ 16-aug-21-04-react-router/src/pages/Loading.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Page from "./Page";
+
+/**
+ * Application loading page component
+ * @returns {JSX}
+ */
+const Loading = () => (
+    <Page>
+        <p>
+            Page is loading
+        </p>
+    </Page>
+);
+
+export default Loading;
