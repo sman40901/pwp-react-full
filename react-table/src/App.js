713b6e33579832824a8686cdfe078e1759e79563
sman40901 <sujanmanpr@gmail.com>
2021-08-30 08:14:57 -0700

    react-table with css

--- react-table/src/App.js
+++ react-table/src/App.js
@@ -1,67 +1,77 @@
-// import React, { useState, useEffect } from "react";
 import React, { useMemo, useState, useEffect } from "react";
-
-import axios from 'axios';
+import axios from "axios";
 
 import Table from "./Table/Table";
 import "./App.css";
 
+const Genres = ({ values }) => {
+  return (
+    <>
+      {values.map((genre, idx) => {
+        return (
+          <span key={idx} className="badge">
+            {genre}
+          </span>
+        );
+      })}
+    </>
+  );
+};
+
 function App() {
-  // data state to store the TV Maze API data. Its initial value is an empty array
+  const columns = useMemo(
+    () => [
+      {
+        Header: "TV Show",
+        columns: [
+          {
+            Header: "Name",
+            accessor: "show.name"
+          },
+          {
+            Header: "Type",
+            accessor: "show.type"
+          }
+        ]
+      },
+      {
+        Header: "Details",
+        columns: [
+          {
+            Header: "Language",
+            accessor: "show.language"
+          },
+          {
+            Header: "Genre(s)",
+            accessor: "show.genres",
+            Cell: ({ cell: { value } }) => <Genres values={value} />
+          },
+          {
+            Header: "Runtime",
+            accessor: "show.runtime",
+            Cell: ({ cell: { value } }) => {
+              const hour = Math.floor(value / 60);
+              const min = Math.floor(value % 60);
+              return (
+                <>
+                  {hour > 0 ? `${hour} hr${hour > 1 ? "s" : ""}` : ""}
+                  {min > 0 ? `${min} min${min > 1 ? "s" : ""}` : ""}
+                </>
+              );
+            }
+          },
+          {
+            Header: "Status",
+            accessor: "show.status"
+          }
+        ]
+      }
+    ],
+    []
+  );
+
   const [data, setData] = useState([]);
 
- 
-    /* 
-    - Columns is a simple array right now, but it will contain some logic later on. 
-    It is recommended by react-table to memoize the columns data
-    - Here in this example, we have grouped our columns into two headers. 
-    react-table is flexible enough to create grouped table headers
-  */
-    const columns = useMemo(
-      () => [
-        {
-          // first group - TV Show
-          Header: "TV Show",
-          // First group columns
-          columns: [
-            {
-              Header: "Name",
-              accessor: "show.name"
-            },
-            {
-              Header: "Type",
-              accessor: "show.type"
-            }
-          ]
-        },
-        {
-          // Second group - Details
-          Header: "Details",
-          // Second group columns
-          columns: [
-            {
-              Header: "Language",
-              accessor: "show.language"
-            },
-            {
-              Header: "Genre(s)",
-              accessor: "show.genres"
-            },
-            {
-              Header: "Runtime",
-              accessor: "show.runtime"
-            },
-            {
-              Header: "Status",
-              accessor: "show.status"
-            }
-          ]
-        }
-      ],
-      []
-    );
-
-     // Using useEffect to call the API once mounted and set the data
   useEffect(() => {
     (async () => {
       const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
@@ -69,10 +79,9 @@ function App() {
     })();
   }, []);
 
-    
   return (
     <div className="App">
-            <Table columns={columns} data={data} />
+      <Table columns={columns} data={data} />
     </div>
   );
 }
