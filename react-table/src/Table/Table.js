a37baa50ccf556329672b4426aa5bf320961ed23
sman40901 <sujanmanpr@gmail.com>
2021-08-30 08:45:39 -0700

    table and search working now

--- react-table/src/Table/Table.js
+++ react-table/src/Table/Table.js
@@ -1,48 +1,71 @@
-import React from "react";
-import { useTable } from "react-table";
+import React, {useState} from "react";
+import { useFilters, useTable, useSortBy } from "react-table";
 
 export default function Table({ columns, data }) {
+
+    // Create a state
+    const [filterInput, setFilterInput] = useState("");
+
+    // Update the state when input changes
+    const handleFilterChange = e => {
+        const value = e.target.value || undefined;
+        setFilter("show.name", value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
+        setFilterInput(value);
+    };
+
     // Use the useTable Hook to send the columns and data to build the table
     const {
         getTableProps, // table props from react-table
         getTableBodyProps, // table body props from react-table
         headerGroups, // headerGroups, if your table has groupings
         rows, // rows for the table based on the data passed
-        prepareRow // Prepare the row (this function needs to be called for each row before getting the row props)
+        prepareRow, // Prepare the row (this function needs to be called for each row before getting the row props)
+        setFilter
     } = useTable({
         columns,
         data
-    });
+    }, useFilters //// Adding the useFilters Hook to the table
+    );
+
 
     /* 
       Render the UI for your table
       - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
     */
     return (
-        <table {...getTableProps()}>
-            <thead>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                            <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row, i) => {
-                    prepareRow(row); // This line is necessary to prepare the rows and get the row props from react-table dynamically
-
-                    // Each row can be rendered directly as a string using the react-table render method
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map(cell => {
-                                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-                            })}
+        // Input element
+        <div className="react-table">
+            <input
+                value={filterInput}
+                onChange={handleFilterChange}
+                placeholder={"Search name"}
+            />
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                            ))}
                         </tr>
-                    );
-                })}
-      </tbody>
-    </table >
-  );
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row, i) => {
+                        prepareRow(row); // This line is necessary to prepare the rows and get the row props from react-table dynamically
+
+                        // Each row can be rendered directly as a string using the react-table render method
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table >
+        </div>
+    );
+
 }
