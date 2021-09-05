bb0d3c2201f7f7033f090709933fa254f52560e5
admin <admin@admins-MacBook-Pro.local>
2021-09-05 08:40:02 -0700

    state mgmt

---
+++ 05-sep-21-01 copy 2/src/component/manatee/Mantee.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Manatee(){
+    return(
+        <h2>Manatee</h2>
+    );
+}
+
+export default Manatee;
---
+++ 05-sep-21-01-state-mgmt/src/components/product/Product.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Product.css';
+
+
+class Product extends Component {
+    render() {
+        return (
+            <div className='wrapper'>
+                <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
+                <div className='addremove'></div>
+                <div className='product'>
+
+                </div>
+                <div className='totalno'>
+
+                </div>
+                <button className='addProduct'>Add</button>
+                <button className='removeProduct'>Remove</button>
+            </div>
+        );
+    }
+}
+
+export default Product;
