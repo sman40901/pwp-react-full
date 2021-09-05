e5025c640170f0317c37fbcf6f2f74bb844f910d
admin <admin@admins-MacBook-Pro.local>
2021-09-05 09:40:58 -0700

    shopping card project added

---
+++ 05-sep-21-01-state-mgmt-shopping-cart/src/components/product/Product.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { useState } from 'react';
+import Cart from '../cart/cart'
+import './Product.css';
+
+
+class Product extends Component {
+    state = {
+        cart: [],
+        total: 0
+    }
+
+    currencyOptions = {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }
+
+    getTotal = () => {
+        return this.state.total.toLocaleString(undefined, this.currencyOptions)
+    }
+
+    addItem = () => {
+        this.setState({
+            cart: ['ice cream'],
+            total: 5
+        })
+    }
+
+    emptyCart = () => {
+        this.setState({
+            cart: [],
+            total: 0
+        })
+    }
+
+
+    render() {
+        return (
+            <div className='wrapper'>
+                <Cart
+                    items={this.state.cart}
+                    total={this.getTotal()}
+                />
+                <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
+                <button
+                    className='btnAddProduct'
+                    onClick={this.addItem}
+                >Add</button>
+                <button
+                    className='btnRemoveProduct'
+                    onClick={this.emptyCart}
+                >Remove</button>
+            </div>
+        );
+    }
+}
+
+export default Product;
