00a527e41e64b4a43ab28b9f4b75cceea83f1f27
sman40901 <sujanmanpr@gmail.com>
2021-09-22 07:32:24 -0700

    forms re

---
+++ 22-sep-21-react-form/src/components/App/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+const handleSubmit = (event) => {
+
+  event.preventDefault();
+  alert('You have submitted the form.')
+
+}
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>How About Them Apples</h1>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Name</p>
+            <input name="name" />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App;
