c9351b42610cb61df2de10e486549c6c6624d03a
sman40901 <sujanmanpr@gmail.com>
2021-09-22 07:52:38 -0700

    apples forms

--- 22-sep-21-react-form/src/components/App/App.js
+++ 22-sep-21-react-form/src/components/App/App.js
@@ -1,22 +1,72 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
-const handleSubmit = (event) => {
-
-  event.preventDefault();
-  alert('You have submitted the form.')
-
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
 }
 
+
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 3000)
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+
   return (
     <div className="wrapper">
       <h1>How About Them Apples</h1>
+      {submitting &&
+        <div>
+          You are submitting the following:
+          <ul>
+            {Object.entries(formData).map(([name, value]) => (
+              <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+            ))}
+          </ul>
+        </div>
+      }
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
             <p>Name</p>
-            <input name="name" />
+            <input name="name" onChange={handleChange} />
+          </label>
+        </fieldset>
+        <fieldset>
+          <label>
+            <p>Apples</p>
+            <select name="apple" onChange={handleChange}>
+              <option value="">--Please choose an option--</option>
+              <option value="fuji">Fuji</option>
+              <option value="jonathan">Jonathan</option>
+              <option value="honey-crisp">Honey Crisp</option>
+            </select>
+          </label>
+          <label>
+            <p>Count</p>
+            <input type="number" name="count" onChange={handleChange} step="1" />
+          </label>
+          <label>
+            <p>Gift Wrap</p>
+            <input type="checkbox" name="gift-wrap" onChange={handleChange} />
           </label>
         </fieldset>
         <button type="submit">Submit</button>
@@ -24,5 +74,4 @@ function App() {
     </div>
   )
 }
-
 export default App;
