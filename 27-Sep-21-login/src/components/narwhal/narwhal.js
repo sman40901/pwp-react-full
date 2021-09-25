9bae8789502bc68d5fbc7ad62f6ab27daf1dce40
sman40901 <sujanmanpr@gmail.com>
2021-09-25 08:51:46 -0700

    login project added

---
+++ 27-Sep-21-login/src/components/narwhal/narwhal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Narwhal(){
+    return(
+        <h2>Narwhal</h2>
+    );
+}
+
+export default Narwhal;
---
+++ 27-Sep-21-login/src/components/whale/beluga.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Beluga(){
+    return(
+        <h2>Beluga</h2>
+    );
+}
+
+export default Beluga;
