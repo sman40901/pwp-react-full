a0f5e9f54768bfb7afd809b8ca069bfe4ddf9332
sman40901 <sujanmanpr@gmail.com>
2021-09-25 20:54:27 -0700

    final project creation

---
+++ reactJS-final-project/src/components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import { render } from 'react-dom';
+// import { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Product from '../product/Product'
+
+//05-sep-2021
+
+function App() {
+  return (
+    <div className="App">
+
+     <Product />
+    </div>
+  );
+}
+
+export default App;
