f7d017663db523855417725a8560514ed6f6489a
admin <admin@admins-MacBook-Pro.local>
2021-09-27 08:15:10 -0700

    login page wip

---
+++ 27-Sep-21-login/src/components/Login/Login.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+function Login() {
+    return (
+        <div className='login'>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text" />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
